e7bb83add625389f60e4d186e2d93fe31db6cf33
Денис Крижановський <denis41391795@knu.ua>
2026-05-22 11:24:53 +0300

    Add unit test for main view

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+from datetime import datetime
+from .models import Recipe, Category
 
-# Create your tests here.
+class RecipeViewsTest(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Test Category")
+        
+        # Створюємо рецепт і примусово встановлюємо дату за 2023 рік 
+        # (оскільки auto_now_add перезаписує дату при створенні)
+        recipe_2023 = Recipe.objects.create(
+            title="Recipe 2023",
+            description="Desc",
+            instructions="Inst",
+            ingredients="Ingr",
+            category=self.category
+        )
+        Recipe.objects.filter(id=recipe_2023.id).update(
+            created_at=timezone.make_aware(datetime(2023, 5, 1))
+        )
+        self.recipe_2023 = Recipe.objects.get(id=recipe_2023.id)
+
+    def test_main_view(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'main.html')
+        self.assertIn(self.recipe_2023, response.context['recipes'])
